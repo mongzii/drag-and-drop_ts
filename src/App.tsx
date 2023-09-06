4b46f2cd2272ec38e0f81380ebd26e53c98f4afa
moonjiyoung <umarg228@naver.com>
2023-09-06 22:45:49 +0900

    feat: drag기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
+  const onDragEnd = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {magic => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {magic => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>⚒️</span>
+                    One
+                  </li>
+                )}
+              </Draggable>
+              {/* <Draggable draggableId="second" index={1}>
+                {magic => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>⚒️</span>
+                    Two
+                  </li>
+                )}
+              </Draggable> */}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
