9005bf98ba79dba656a62eb3103e53542eb40682
moonjiyoung <umarg228@naver.com>
2023-09-07 22:30:21 +0900

    feat: drag에 따라 css설정

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,21 @@ const Boards = styled.div`
   grid-template-columns: repeat(1, 1fr);
 `;
 const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
   background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
 `;
 
 const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
   background-color: ${props => props.theme.cardColor};
 `;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
   const onDragEnd = () => {};
@@ -34,17 +43,19 @@ function App() {
           <Droppable droppableId="one">
             {magic => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
-                <Draggable draggableId="first" index={0}>
-                  {magic => (
-                    <Card
-                      ref={magic.innerRef}
-                      {...magic.dragHandleProps}
-                      {...magic.draggableProps}
-                    >
-                      One
-                    </Card>
-                  )}
-                </Draggable>
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {magic => (
+                      <Card
+                        ref={magic.innerRef}
+                        {...magic.dragHandleProps}
+                        {...magic.draggableProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
                 {/* <Draggable draggableId="second" index={1}>
                 {magic => (
                   <li ref={magic.innerRef} {...magic.draggableProps}>
@@ -53,6 +64,8 @@ function App() {
                   </li>
                 )}
               </Draggable> */}
+                {magic.placeholder}
+                {/* 카드빼도 boards의 사이즈 변하지 않는다, */}
               </Board>
             )}
           </Droppable>
